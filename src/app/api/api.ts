916c3edc9c8e9c239ab880b80ab6a4001383577a
PACIFICABS\bhargav.lakhani <bhargav.lakhani@technomark.io>
2024-06-27 11:56:42 +0530

    set public url

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = process.env.api_url;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const getdata = async (param: string) => {
   console.log("apiEndpoint", API_URL);
 
