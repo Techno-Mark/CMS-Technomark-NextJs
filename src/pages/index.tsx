6f711f0a5861e868c7dd794e83afd54fa4ee901a
Alvish6654 <alvish.pipaliya@technomark.io>
2024-10-15 15:02:06 +0530

    change in seo code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import Loading from "@/components/common/loading/loading"
 import { useRouter } from "next/navigation"
-import React, { useEffect } from "react"
+import { useEffect } from "react"
 
 const Index = () => {
   const router = useRouter()
@@ -8,7 +7,6 @@ const Index = () => {
   useEffect(() => {
     router.push("/home")
   }, [router])
-  return;
 }
 
 export default Index
