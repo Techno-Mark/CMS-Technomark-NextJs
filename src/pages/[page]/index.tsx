943ceb53e3e181c1e2d8344cb582d13ef105303f
Alvish6654 <alvish.pipaliya@technomark.io>
2024-10-16 17:41:51 +0530

    seo static script add

--- src/pages/[page]/index.tsx
+++ src/pages/[page]/index.tsx
@@ -43,11 +43,14 @@ const apiCall = async (
 
 const seoData = async () => {
   try {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/seo-script`, {
-      headers: {
-        referal: process.env.REFERAL_HEADER || ""
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/seo-script`,
+      {
+        headers: {
+          referal: process.env.REFERAL_HEADER || ""
+        }
       }
-    })
+    )
     return res.data.data
   } catch (error) {
     console.error(`Error fetching  data:`, error)
@@ -57,11 +60,14 @@ const seoData = async () => {
 
 const ThemeData = async () => {
   try {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/theme-data`, {
-      headers: {
-        referal: process.env.REFERAL_HEADER || ""
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/theme-data`,
+      {
+        headers: {
+          referal: process.env.REFERAL_HEADER || ""
+        }
       }
-    })
+    )
     return res.data.data
   } catch (error) {
     console.error(`Error fetching  data:`, error)
@@ -119,20 +125,62 @@ const Page: React.FC<
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
         {/* Inject Google Analytics beforeScript */}
-        {seo?.googleAnalytics?.beforeScript && (
+        {/* {seo?.googleAnalytics?.beforeScript && (
           <script
             dangerouslySetInnerHTML={{
               __html: seo.googleAnalytics.beforeScript
             }}
           />
-        )}
+        )} */}
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(w, d, s, l, i){
+          w[l] = w[l] || [];
+          w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+          var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != 'dataLayer' ? '&l=' + l : '';
+          j.async = true;
+          j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-K4V6FNWF');
+      `
+          }}
+        />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8P5JMP37TK"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { window.dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-8P5JMP37TK');
+    `
+          }}
+        />
 
         {/* Inject Facebook Script */}
-        {seo?.facebookScript && (
+        {/* {seo?.facebookScript && (
           <script dangerouslySetInnerHTML={{ __html: seo.facebookScript }} />
-        )}
+        )} */}
       </Head>
       <ToastContainer />
+
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-K4V6FNWF"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
 
       {/* <style jsx global>{`
         :root {
@@ -142,11 +190,11 @@ const Page: React.FC<
       `}</style> */}
 
       {/* Inject Google Analytics afterScript */}
-      {seo?.googleAnalytics?.afterScript && (
+      {/* {seo?.googleAnalytics?.afterScript && (
         <script
           dangerouslySetInnerHTML={{ __html: seo.googleAnalytics.afterScript }}
         />
-      )}
+      )} */}
       {data?.popups &&
         !!data.popups.length &&
         data.popups?.map((popupDetail: any, index: number) => (
